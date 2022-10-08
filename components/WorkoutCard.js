1ed677a5e52de2cd90092c2a4ca5e7e34a32d7a1
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2022-10-08 17:49:01 +0200

    [WIP] createExercise

--- components/WorkoutCard.js
+++ components/WorkoutCard.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
-import { Pressable, Flex, Text, NativeBaseProvider } from 'native-base';
+import { Pressable, Flex, Text } from 'native-base';
 
 const WorkoutCard = (props) => {
   return (
-    <NativeBaseProvider>
-      <Pressable
-        onPress={() => props.navigation.navigate('Workout', { workout: props.workout })}
-        rounded="8"
-        borderColor="coolGray.300"
-        maxW="96"
-        shadow="5"
-        bg="coolGray.100"
-        p="10"
-      >
-        <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize={16}>
-          {props.workout.name}
+    <Pressable
+      onPress={() => props.navigation.navigate('Workout', { workout: props.workout })}
+      rounded="8"
+      borderColor="coolGray.300"
+      maxW="96"
+      shadow="5"
+      bg="coolGray.100"
+      p="10"
+    >
+      <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize={16}>
+        {props.workout.name}
+      </Text>
+      <Flex>
+        <Text color="darkBlue.600" mt="2" fontWeight="medium" fontSize={12}>
+          {props.workout.lastDate}
         </Text>
-        <Flex>
-          <Text color="darkBlue.600" mt="2" fontWeight="medium" fontSize={12}>
-            {props.workout.lastDate}
-          </Text>
-        </Flex>
-      </Pressable>
-    </NativeBaseProvider>
+      </Flex>
+    </Pressable>
   );
 };
 
--- screens/CreateWorkout.js
+++ screens/CreateWorkout.js
@@ -1,23 +1,114 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Input, Stack } from 'native-base';
-
-import CreateExercise from '../components/CreateExercise';
+import React, { useRef, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView } from 'react-native';
+import { Box, Input, Pressable, Text, ScrollView, HStack, Button } from 'native-base';
+import { AntDesign } from '@expo/vector-icons';
 
 const CreateWorkout = () => {
+  const [textValue, setTextValue] = useState('');
+  const [numInputs, setNumInputs] = useState(1);
+  const refInputs = useRef([textValue]);
+
+  const setInputValue = (index, value) => {
+    const inputs = refInputs.current;
+    inputs[index] = value;
+    setTextValue(value);
+  };
+
+  const removeInput = (i) => {
+    refInputs.current.splice(i, 1)[0];
+    setNumInputs((value) => value - 1);
+  };
+
+  const addInput = () => {
+    refInputs.current.push('');
+    setNumInputs((value) => value + 1);
+  };
+
+  const scrollViewRef = useRef();
+
+  const inputs = [];
+
+  for (let i = 0; i < numInputs; i++) {
+    inputs.push(
+      <View key={i} style={{ flexDirection: 'row', alignItems: 'center', margin: 5 }}>
+        <HStack style={styles.stack}>
+          <Text style={styles.text}>{i + 1}.</Text>
+          <Input
+            style={styles.input}
+            variant="unstyled"
+            onChangeText={(value) => setInputValue(i, value)}
+            value={refInputs.current[i]}
+            placeholder="Exercise"
+          />
+          <Pressable onPress={() => removeInput(i)}>
+            <AntDesign name="minuscircleo" size={20} color="red" />
+          </Pressable>
+        </HStack>
+      </View>
+    );
+  }
+
   return (
-    <Stack style={styles.containerView} w="100%" alignItems="center">
-      <Input variant="rounded" placeholder="Workout" />
-      <CreateExercise />
-    </Stack>
+    <KeyboardAvoidingView
+      style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}
+      behavior="position"
+      enabled
+      keyboardVerticalOffset={100}
+    >
+      <ScrollView
+        contentContainerStyle={styles.content}
+        ref={scrollViewRef}
+        onContentSizeChange={() => scrollViewRef.current?.scrollToEnd({ animated: true })}
+      >
+        <View style={styles.view}>
+          <Box style={styles.box}>
+            <Input variant="rounded" w="75%" mx="auto" placeholder="Workout name" />
+          </Box>
+          {inputs}
+          <Button variant="rounded" style={styles.button} onPress={addInput}>
+            <Text style={styles.buttonText}>Add Exercise</Text>
+          </Button>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  containerView: {
-    flex: 1,
+  content: {
     alignItems: 'center',
-    justifyContent: 'center'
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  view: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  input: {
+    maxWidth: '80%'
+  },
+  button: {
+    margin: 20,
+    backgroundColor: 'cyan'
+  },
+  text: {
+    color: 'black',
+    width: 20,
+    marginLeft: 15
+  },
+  buttonText: {
+    color: 'white'
+  },
+  box: {
+    margin: 15
+  },
+  stack: {
+    flex: 0.9,
+    alignItems: 'center',
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 30,
+    padding: 10
   }
 });
 
