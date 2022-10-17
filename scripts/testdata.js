734d8d06b5ec036980003ad1743c84b9100358bf
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2022-10-17 23:19:56 +0200

    edited testdata

--- scripts/testdata.js
+++ scripts/testdata.js
@@ -1,114 +1,366 @@
 const testdata = () => {
-  return ([
+  return [
     {
-      name: 'TestWorkout1',
+      name: 'Chest-Bizeps-Day',
       lastDate: '22.22.2222',
-      exercises: {
-        benchpress: {},
-        butterfly: {},
-        bizepsCurls: {}
-      }
+      exercises: [
+        {
+          benchpress: [
+            {
+              date: '22.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '21.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          butterfly: [
+            {
+              date: '22.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '21.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          bizepsCurls: [
+            {
+              date: '22.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '21.22.2222',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        }
+      ]
     },
     {
-      name: 'TestWorkout2',
+      name: 'Back-Day',
       lastDate: '11.11.1111',
-      exercises: {
-        textExercise21: {},
-        textExercise22: {},
-        textExercise23: {}
-      }
+      exercises: [
+        {
+          pullup: [
+            {
+              date: '11.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '10.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          superman: [
+            {
+              date: '11.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '10.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          deadlift: [
+            {
+              date: '11.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '10.11.1111',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        }
+      ]
     },
     {
       name: 'TestWorkout3',
-      lastDate: '00.00.0000',
-      exercises: {
-        textExercise31: {},
-        textExercise32: {},
-        textExercise33: {}
-      }
-    },
-    {
-        name: 'TestWorkout4',
-        lastDate: '22.22.2222',
-        exercises: {
-          benchpress: {},
-          butterfly: {},
-          bizepsCurls: {}
+      lastDate: '01.00.0000',
+      exercises: [
+        {
+          pullup: [
+            {
+              date: '01.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '00.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          superman: [
+            {
+              date: '01.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '00.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
+        },
+        {
+          deadlift: [
+            {
+              date: '01.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            },
+            {
+              date: '00.00.0000',
+              sets: {
+                1: {
+                  weigh: 20,
+                  repeats: 8
+                },
+                2: {
+                  weigh: 25,
+                  repeats: 6
+                },
+                3: {
+                  weigh: 30,
+                  repeats: 6
+                }
+              }
+            }
+          ]
         }
-      },
-      {
-        name: 'TestWorkout5',
-        lastDate: '11.11.1111',
-        exercises: {
-          textExercise21: {},
-          textExercise22: {},
-          textExercise23: {}
-        }
-      },
-      {
-        name: 'TestWorkout6',
-        lastDate: '00.00.0000',
-        exercises: {
-          textExercise31: {},
-          textExercise32: {},
-          textExercise33: {}
-        }
-      },
-      {
-        name: 'TestWorkout7',
-        lastDate: '22.22.2222',
-        exercises: {
-          benchpress: {},
-          butterfly: {},
-          bizepsCurls: {}
-        }
-      },
-      {
-        name: 'TestWorkout8',
-        lastDate: '11.11.1111',
-        exercises: {
-          textExercise21: {},
-          textExercise22: {},
-          textExercise23: {}
-        }
-      },
-      {
-        name: 'TestWorkout9',
-        lastDate: '00.00.0000',
-        exercises: {
-          textExercise31: {},
-          textExercise32: {},
-          textExercise33: {}
-        }
-      },
-      {
-        name: 'TestWorkout10',
-        lastDate: '22.22.2222',
-        exercises: {
-          benchpress: {},
-          butterfly: {},
-          bizepsCurls: {}
-        }
-      },
-      {
-        name: 'TestWorkout11',
-        lastDate: '11.11.1111',
-        exercises: {
-          textExercise21: {},
-          textExercise22: {},
-          textExercise23: {}
-        }
-      },
-      {
-        name: 'TestWorkout12',
-        lastDate: '00.00.0000',
-        exercises: {
-          textExercise31: {},
-          textExercise32: {},
-          textExercise33: {}
-        }
-      }
-  ]);
+      ]
+    }
+  ];
 };
 
 export default testdata;
