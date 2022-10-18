6730da4d5441741ebf11325c42d067b6adb02b39
kaufi95 <60215458+kaufi95@users.noreply.github.com>
2022-10-18 23:08:00 +0200

    linked delete method to home-screen menu

--- components/WorkoutMenu.js
+++ components/WorkoutMenu.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { Pressable, View, Text, Menu, Box, ThreeDotsIcon } from 'native-base';
+import { Alert } from 'react-native';
+import { Pressable, Text, Menu, ThreeDotsIcon } from 'native-base';
+
+import uuid from 'react-native-uuid';
+
+import { removeWorkout } from '../scripts/storage';
 
 const WorkoutCard = (props) => {
   return (
     <Menu
       trigger={(triggerProps) => {
         return (
-          <Pressable accessibilityLabel="More options menu" {...triggerProps}>
-            <ThreeDotsIcon />
+          <Pressable {...triggerProps}>
+            <ThreeDotsIcon size={8} />
           </Pressable>
         );
       }}
     >
-      <Menu.Item>Edit</Menu.Item>
-      <Menu.Item>Delete</Menu.Item>
+      <Menu.Item>
+        <Text>Edit</Text>
+      </Menu.Item>
+      <Menu.Item
+        onPress={() =>
+          Alert.alert('Delete', 'Are you sure you want to delete this Workout?', [
+            {
+              text: 'Cancel',
+              style: 'cancel'
+            },
+            { text: 'DELETE IT!', onPress: () => removeWorkout(props.workout) }
+          ])
+        }
+      >
+        Delete
+      </Menu.Item>
     </Menu>
   );
 };
