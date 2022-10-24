754b15ca3b14c46901841c16ef7b6e1e1ad4830d
yce5586 <yce5586@students.fhv.at>
2022-10-24 20:01:12 +0200

    front-end unterer teil von do exercise

--- screens/ExerciseScreen.js
+++ screens/ExerciseScreen.js
@@ -23,71 +23,81 @@ const ExerciseScreen = (props) => {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       enabled
       style={[
-        styles.container,
-        {
-          // Try setting flexDirection to "row".
-          flexDirection: "column",
-        },
-      ]}
-    >
+        {flex: 1, flexDirection: "column"}]}>
       <View style={{ flex: 3, backgroundColor: "red" }}>
-        <Text>irgendwas</Text>
+        {/*Hier kommem die Workouts vom letzten Mal rein*/} 
       </View>
 
       <View style={{ flex: 2, flexDirection: "column" }}>
 
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: "row", paddingTop: 20}}>
 
-          <View style={styles.IconStyleView} />
-            <View style={styles.IconStyleView}>
-              <Icon name="weight-hanging" size={30} style={styles.IconStyle} />
-            </View>
-            <View style={styles.TextInputView}>
-              <FeatherIcon name="repeat" size={30} style={styles.IconStyle} />
-            </View>
+        <View style={{flexDirection: "column", flex: 2}}/>
+
+          <View style={{flexDirection: "column", flex: 4}}>
+            <TextInput
+              mode="outlined"
+              label = <Icon name="weight-hanging" size={30} style={{alignSelf: "center"}} />
+            />
+          </View>
+
+          <View style={{flexDirection: "column", flex: 2}}/>
+
+          <View style={[styles.TextInputView, { flex: "4"}]}>
+            <TextInput
+              mode="outlined"
+              label = <FeatherIcon name="repeat" size={30} style={{alignSelf: "center"}} />
+            />
+          </View>
+
+          <View style={{flexDirection: "column", flex: 2}}/>
+
         </View>
 
-        <View style={{ flexDirection: "row" }}>
-          <View style={ {flex: 1}}>
-            <Text style={{ marginTop: "auto", marginBottom: "auto", marginLeft: 10 }}>
+        <View style={{ flexDirection: "row", paddingTop: 20}}>
+
+          <View style={{flexDirection: "column", flex: 2}}/>
+
+          <View style={{flexDirection: "column", flex: 4}}>
+            <AntIcon
+              name="caretleft"
+              size={55}
+              onPress={() => {
+                console.log("links wurde gedrücken");
+              }}
+              style={{alignSelf: "center"}}
+            />
+          </View>
+
+          <View style={{flexDirection: "column", flex: 2}}>
+            <Text style={{ marginTop: "auto", marginBottom: "auto", fontSize: 24}}>
               1. Set
             </Text>
           </View>
 
-          <View style={styles.TextInputView}>
-            <TextInput style={styles.TextInput} placeholder="Weight" />
+          <View style={{flexDirection: "column", flex: 4}}>
+            <AntIcon
+              name="caretright"
+              size={55}
+              onPress={() => {
+                console.log("rechts wurde gedrücken");
+              }}
+              style={{ alignSelf: "center" }}
+            />
           </View>
 
-          <View style={[styles.TextInputView, {alignItems: "flex-start"}]}>
-            <TextInput style={styles.TextInput} placeholder="Reps" />
+          <View style={{flexDirection: "column", flex: 2}}/>
+          
+        </View>
+
+        <View style={{flexDirection: "row", alignSelf: "center"}}>
+
+          <View style={{flexDirection: "column"}}>
+            <Button mode="contained" style={{alignSelf: "center", justifyContent: "center", borderRadius: 10, padding: 10, marginTop: 20}}>
+              Finish Exercise
+            </Button>
           </View>
-        </View>
-
-        <View
-          style={{ flexDirection: "row", alignSelf: "center", marginTop: 10 }}
-        >
-          <AntIcon
-            name="caretleft"
-            size={55}
-            onPress={() => {
-              console.log("links wurde gedrücken");
-            }}
-            style={{ marginRight: 15 }}
-          />
-          <AntIcon
-            name="caretright"
-            size={55}
-            onPress={() => {
-              console.log("rechts wurde gedrücken");
-            }}
-            style={{ marginLeft: 15 }}
-          />
-        </View>
-
-        <View style={{ flexDirection: "row", alignSelf: "center" }}>
-          <Button mode="contained" style={styles.Button}>
-            Finish Exercise
-          </Button>
+          
         </View>
       </View>
     </KeyboardAvoidingView>
@@ -95,39 +105,9 @@ const ExerciseScreen = (props) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  TextInputView: {
-    flex: 4,
-    flexDirection: "column",
-    alignItems: "center",
-  },
 
-  IconStyleView:{
-    flex: 4,
-    flexDirection: "column",
-    alignItems: "flex-start"
-   
-  },
-
-  TextInput: {
-    width: 100,
-    borderWidth: 1,
-    marginBottom: "auto",
-    marginTop: 10,
-  },
-  IconStyle: {
-    alignItems: "flex-start",
-    marginTop: 10,
-  },
-  Button: {
-    alignSelf: "center",
-    justifyContent: "center",
-    borderRadius: 10,
-    padding: 10,
-    marginTop: 35,
-  },
 });
 
 export default ExerciseScreen;
+
+
